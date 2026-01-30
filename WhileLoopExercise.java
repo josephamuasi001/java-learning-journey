bd7226d15c1e1dcbb9bf080a92bc80960151bf7f
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-01-30 18:13:18 +0000

    Add while loop exercise printing numbers 1 to 10 and vice versa

---
+++ WhileLoopExercise.java
@@ -0,0 +1,17 @@
+public class WhileLoopExercise {
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number <= 10) {
+            System.out.println("Number: " + number);
+            number++;
+        }
+
+        int number2 = 10;
+        
+        while (number2 >= 1) {
+            System.out.println("Number: " + number2);
+            number2--;
+        }
+    }
+}
