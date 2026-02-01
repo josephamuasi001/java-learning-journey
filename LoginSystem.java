3da8d72b9ca0c9e1574cd5bad0a05723a2902931
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-01 23:01:39 +0000

    Revert to original

--- LoginSystem.java
+++ LoginSystem.java
@@ -6,29 +6,28 @@ public class LoginSystem {
         String correctUsername = "admin";
         String correctPassword = "1234";
         int counter = 3;
-        boolean loggedIn = false;
+        boolean loggedIn = true;
+    
 
         while (counter > 0) {
             System.out.println("Enter your username: ");
             String userName = scanner.nextLine();
-
             System.out.println("Enter your password: ");
             String password = scanner.nextLine();
-
-            if (userName.equalsIgnoreCase(correctUsername)
-                    && password.equalsIgnoreCase(correctPassword)) {
-                System.out.println("Login successful!");
+            
+            if (userName.equalsIgnoreCase(correctUsername) && password.equalsIgnoreCase(correctPassword)) {
+                System.out.println("Login successful !");
                 loggedIn = true;
-                break; // exit loop immediately
-            } else {
-                counter--;
+                break;
+            } else  {
+                counter -= 1;
                 System.out.println("Invalid credentials");
                 System.out.println("You have " + counter + " attempts remaining");
-            }
-        }
 
+            }
+        }    
         if (!loggedIn) {
-            System.out.println("Account locked. No attempts remaining.");
-        }
+            System.out.println("Account locked, you have " + counter + " attempts!");
+        }       
     }
 }
