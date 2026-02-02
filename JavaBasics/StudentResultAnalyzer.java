4b306be47bee07dd3cdd2534aee9938b6b913b0b
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-02 23:39:13 +0000

    Extra push

--- JavaBasics/StudentResultAnalyzer.java
+++ JavaBasics/StudentResultAnalyzer.java
@@ -4,7 +4,16 @@ public class StudentResultAnalyzer {
     Scanner scanner = new Scanner(System.in);
     boolean running = true;
     while (running) {
+        System.out.println("Enter student name here: ");
+        String name = scanner.nextLine();
         
+        System.out.println("How many subjects (1-6): ");
+        int noOfSubjects = scanner.nextInt();
+
+        for(int i = 1; i <= noOfSubjects; i++ ) {
+            System.out.println("Enter " + name + "'s " + i + "score here");
+        }
+
     }
 
 
