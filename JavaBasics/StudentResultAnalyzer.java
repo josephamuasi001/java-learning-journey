0de845d6fb9bd1aa1e6e585c506ee5bda2822754
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-02 23:48:51 +0000

    I am stuck here

--- JavaBasics/StudentResultAnalyzer.java
+++ JavaBasics/StudentResultAnalyzer.java
@@ -1,20 +1,35 @@
 package JavaBasics;
 import java.util.Scanner;
-public class StudentResultAnalyzer {
-    Scanner scanner = new Scanner(System.in);
-    boolean running = true;
-    while (running) {
-        System.out.println("Enter student name here: ");
-        String name = scanner.nextLine();
-        
-        System.out.println("How many subjects (1-6): ");
-        int noOfSubjects = scanner.nextInt();
 
-        for(int i = 1; i <= noOfSubjects; i++ ) {
-            System.out.println("Enter " + name + "'s " + i + "score here");
+public class StudentResultAnalyzer {
+    public static void main(String[] args) {
+        
+    
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        int noOfSubjects;
+        int totalScore = 0;
+        int averageScore = (totalScore) / (noOfSubjects);
+
+        while (running) {
+            System.out.println("Enter student name here: ");
+            String name = scanner.nextLine();
+        
+            System.out.println("How many subjects (1-6): ");
+            noOfSubjects = scanner.nextInt();
+
+            for(int i = 1; i <= noOfSubjects; i++ ) {
+                System.out.println("Enter " + name + "'s " + i + "score here (0-100): ");
+                int studentScore = scanner.nextInt();
+                scanner.nextLine();
+                if (studentScore < 0 || studentScore > 100) {
+                    System.out.println("Invalid score! ");
+                    System.out.println("Re-enter score here: ");
+                }
+
+            }
+
         }
 
     }
-
-
 }
