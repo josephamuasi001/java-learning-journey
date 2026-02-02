378cdfcbcaff9b84faf20aacbfad3c7808f65594
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-02 07:40:56 +0000

    Remove deleted files

--- DoWhileExercise.java
+++
@@ -1,9 +0,0 @@
-public class DoWhileExercise {
-    public static void main(String[] args) {
-        int number = 1;
-        do {
-            System.out.println(number);
-            number++;
-        } while (number <= 5);
-    }
-}
--- DoWhileLoops.java
+++
@@ -1,9 +0,0 @@
-public class DoWhileLoops {
-    public static void main(String[] args) {
-        int number = 1;
-        do {
-            System.out.println("Number: " + number);
-            number++;
-        } while (number <= 10);
-    }
-}
