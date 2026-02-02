d80eba3d91cc41d1d4d0a0ea320830e4ca072026
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-02 23:33:11 +0000

    Another prohect

---
+++ JavaBasics/StudentResultAnalyzer.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+import java.util.Scanner;
+public class StudentResultAnalyzer {
+    Scanner scanner = new Scanner(System.in);
+    boolean running = true;
+    while (running) {
+        
+    }
+
+
+}
