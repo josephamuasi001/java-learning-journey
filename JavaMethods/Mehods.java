c270f0584a260eebe00c9e6b51a3487a0c88caef
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-10 02:44:06 +0000

    First exercise in methods

--- JavaMethods/Mehods.java
+++ JavaMethods/Mehods.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Mehods {
     public static void main(String[] args) {
-        sayHello("Joseph", 89);
-
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter your name here: ");
+        String nameU = scanner.nextLine();
+        System.out.println("Enter your age here: ");
+        int ageU = scanner.nextInt();
+        sayHello(nameU, ageU);
+        scanner.close();
     }
     public static void sayHello(String name, double age) {
         System.out.println("Hello " + name );
---
+++ JavaMethods/MethodsExercise1.java
@@ -0,0 +1,15 @@
+public class MethodsExercise1 {
+    public static void main(String[] args) {
+        showAge(20);
+        doubleNumber(30);
+    }
+
+    public static void showAge(int age) {
+        System.out.println("Your age is " + age);
+    }
+
+    public static void doubleNumber(double number) {
+        double douNumber = number; 
+        System.out.println("Double number: " + douNumber);
+    }
+}
