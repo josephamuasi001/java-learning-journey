28aa58879fea06b44e6e63e2d5c261a9048d6a8a
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-10 03:06:39 +0000

    New update to my file

--- JavaMethods/NewExercise.java
+++ JavaMethods/NewExercise.java
@@ -7,11 +7,16 @@ public class NewExercise {
         int j = 8;
         boolean status = isAdult(j);
         System.out.println("You are an adult: " + status);
+
+        int t = 13;
+        String temperature = checkTemperature(t);
+        System.out.println("Your temperature is " + temperature);
     }
+    // Square number method
     public static int squareNumber(int num1) {
         return (num1 * num1 );
     }
-
+    //Method - isAdult
     public static boolean isAdult(int age) {
         if (age >= 18 ) {
             return true;
@@ -19,7 +24,7 @@ public class NewExercise {
             return false;
         }
     }
-    
+
 // Temperature method
     public static String checkTemperature(int temp) {
         if (temp >= 30) {
