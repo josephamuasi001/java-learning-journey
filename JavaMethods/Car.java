7d315702db4fe6af9f517177bf7678481f8b9b3e
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-10 15:43:02 +0000

    Added a new file

--- JavaMethods/Car.java
+++ JavaMethods/Car.java
@@ -1,12 +1,4 @@
 public class Car {
-    public static void main(String[] args) {
-        Car c1 = new Car("HVP2", 89);
-    }
-
     String brand;
     int year;
-    public void displayCarInfo(String name, int year) {
-        System.out.println("Brand : " + brand);
-        System.out.println("Year : " + year);
-    }
 }
