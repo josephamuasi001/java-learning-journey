0b54e9a805fcfd2e05ae529a6ba72ab8811eda3c
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-02-11 07:13:45 +0000

    New changes

---
+++ JavaMethods/Product.java
@@ -0,0 +1,58 @@
+public class Product {
+    private String productID;
+    private String name;
+    private double price;
+    private int quantityInStock;
+
+    public Product(String productID, String name, double price, int quantityInStock) {
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+        this.quantityInStock = quantityInStock;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+    
+    public boolean isInStock(int quantityInStock) {
+        if (quantityInStock > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void updateStock(int quantitySold) {
+        if (quantitySold <= quantityInStock) {
+            quantityInStock -= quantitySold;
+        } else {
+            System.out.println("Not enough stock to sell " + quantitySold + " units.");
+        }
+    }
+
+}
+
+
